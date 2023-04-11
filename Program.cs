9c47a6a6673772b3640ef435d3623a943b117f6f
Gustavo Nozaki Soares <gnozakisoares@gmail.com>
2023-04-11 20:42:35 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string path = @"c:\Curso - C#\Exercicios - C#\Seção 14\in.txt";
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        List<string> list = new List<string>();
+        while (!sr.EndOfStream)
+        {
+            list.Add(sr.ReadLine());
+        }
+        list.Sort();
+        foreach (string str in list)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error accuried");
+    Console.WriteLine(e.Message);
+}
